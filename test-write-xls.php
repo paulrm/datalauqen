535f83ed1dbd673c4ae2b66fb72afe7f7e09fe52
paul-neuroglia <paul@neuroglia.com.ar>
2017-08-05 13:02:34 -0300

    pruebas de lectura ok

--- test-write-xls.php
+++ test-write-xls.php
@@ -96,6 +96,7 @@ echo date('H:i:s') , ' Current memory usage: ' , (memory_get_usage(true) / 1024 
 
 
 // Save Excel 95 file
+/*
 echo date('H:i:s') , " Write to Excel5 format" , EOL;
 $callStartTime = microtime(true);
 
@@ -106,6 +107,9 @@ $callTime = $callEndTime - $callStartTime;
 
 echo date('H:i:s') , " File written to " , str_replace('.php', '.xls', pathinfo(__FILE__, PATHINFO_BASENAME)) , EOL;
 echo 'Call time to write Workbook was ' , sprintf('%.4f',$callTime) , " seconds" , EOL;
+ */
+  
+  
 // Echo memory usage
 echo date('H:i:s') , ' Current memory usage: ' , (memory_get_usage(true) / 1024 / 1024) , " MB" , EOL;
 
